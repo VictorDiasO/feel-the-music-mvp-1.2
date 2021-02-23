e2cc2f4431cc0d2caace3aac6dd9ffab0e2d3aa3
VictorDiasO <victordiasdeveloper@gmail.com>
2021-02-23 01:28:16 -0300

    vibration test 4 (changing to 400ms) | 01:28 | 22/02/2021

--- src/components/Canvas.js
+++ src/components/Canvas.js
@@ -93,7 +93,7 @@ class Canvas extends Component {
         console.log('2: ', data2);
     }
 
-    vibrate = () => window.navigator.vibrate(200);
+    vibrate = () => window.navigator.vibrate(400);
 
     vibrating(x, end_x, y, end_y) {
         const magicNumber = 450.3692930842692;
